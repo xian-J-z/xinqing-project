b6d42f5a3d991310989b275ea5a1d71531bcb76f
muted1235 <3459436139@qq.com>
2026-04-25 10:06:37 +0800

    修复图片加载问题

--- miniprogram/pages/article/list.js
+++ miniprogram/pages/article/list.js
@@ -29,10 +29,29 @@ Page({
         }
       })
       if (res.result) {
-        const articles = (res.result.data || []).map(a => ({
+        let articles = res.result.data || []
+        
+        // 批量转换封面图片 cloud:// 路径为 https:// 临时链接
+        const coverUrls = articles.map(a => a.cover).filter(url => url && url.startsWith('cloud://'))
+        if (coverUrls.length > 0) {
+          const tempFileURLs = await Promise.all(
+            coverUrls.map(url => wx.cloud.getTempFileURL({ fileID: url }))
+          )
+          const urlMap = {}
+          coverUrls.forEach((url, index) => {
+            urlMap[url] = tempFileURLs[index].tempFileURL || url
+          })
+          articles = articles.map(a => ({
+            ...a,
+            cover: urlMap[a.cover] || a.cover
+          }))
+        }
+        
+        articles = articles.map(a => ({
           ...a,
           displayTime: this.formatTime(a.updateTime || a.createTime)
         }))
+        
         this.setData({
           articles: articles,
           total: res.result.total || articles.length,
@@ -41,6 +60,7 @@ Page({
         })
       }
     } catch (e) {
+      console.error('加载文章失败', e)
       this.setData({ loading: false })
       wx.showToast({ title: '加载失败', icon: 'none' })
     }
